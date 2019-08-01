c58aa1f5c11ff4b3b238230cbcaf5562ac807093
Luciano Santos <luciano@debianX.debianX>
2019-08-01 11:21:57 -0300

    Created server for app

---
+++ server/src/main/java/com/okta/developer/demo/DemoApplication.java
@@ -0,0 +1,32 @@
+package com.okta.developer.demo;
+
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+import java.util.stream.Stream;
+
+@EnableResourceServer
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Bean
+	ApplicationRunner init(CarRepository repository){
+		return args -> {
+			Stream.of("Ferrari", "Jaguar", "Porsche", "Lamborghini", "Bugatti",
+					"AMC Gremlin", "Triumph Stag", "Ford Pinto", "Yugo GV").forEach(name ->{
+						Car car = new Car();
+						car.setName(name);
+						repository.save(car);
+			});
+			repository.findAll().forEach(System.out::println);
+		};
+	}
+
+}
